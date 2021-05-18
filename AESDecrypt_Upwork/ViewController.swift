5f23f94958d0cd1daafd7404a3504f24fe789a98
Aung Khant Myo <twizzy.indy@gmail.com>
2021-05-18 17:25:00 +0630

    Initial Commit

---
+++ AESDecrypt_Upwork/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AESDecrypt_Upwork
+//
+//  Created by twizzyindy on 18/05/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
